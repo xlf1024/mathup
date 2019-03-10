a487bae6298f0c625f4fefc3a8efbf8c9d8f4742
Rúnar Berg <runar.BaugssonSigridarson@netapp.com>
2019-03-09 19:16:18 -0800

    v0.6.2-dist

--- dist/ascii2mathml.js
+++ dist/ascii2mathml.js
@@ -1,6 +1,12 @@
-/*! ascii2mathml v0.6.1 | (c) 2015 (MIT) | https://github.com/runarberg/ascii2mathml#readme */
+/*! ascii2mathml v0.6.2 | (c) 2015 (MIT) | https://github.com/runarberg/ascii2mathml#readme */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ascii2mathml = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ascii2mathml = ascii2mathml;
+exports.default = void 0;
 
 var _parser = _interopRequireDefault(require("./lib/parser"));
 
@@ -75,7 +81,8 @@ function ascii2mathml(asciimath, options) {
   return out;
 }
 
-module.exports = ascii2mathml;
+var _default = ascii2mathml;
+exports.default = _default;
 
 },{"./lib/parser":3}],2:[function(require,module,exports){
 "use strict";
